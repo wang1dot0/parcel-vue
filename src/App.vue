62a7a0706f33ef70d72a4c396011425e0350268a
wangguansheng <wang.guansheng@navercorp.com>
2019-02-01 11:30:21 +0800

    basic demo

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Hello, {{message}}</h2>
+    <hello />
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue';
+
+export default {
+  name: 'App',
+  components: { Hello },
+  data() {
+    return {
+      message: 'Parcel!',
+    };
+  },
+};
+</script>
+
+<style>
+  div {
+    margin: 0 auto;
+    width: 800px;
+  }
+  h2 {
+    text-align: center;
+    margin-bottom: 50px;
+  }
+</style>
+
---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h4>Tips</h4>
+    <ul>
+      <li>This is the first tip.</li>
+      <li>And this is the second one.</li>
+    </ul>
+  </div>
+</template>
+
+<<script>
+export default {
+  name: 'Hello',
+};
+</script>
+
+<style>
+  .hello {
+    color: #666;
+  }
+  h4 {
+    font-weight: 400;
+  }
+  ul {
+    list-style-type: circle;
+    padding-inline-start: 20px;
+  }
+  ul li:nth-of-type(1) {
+    color: green;
+  }
+</style>
+
