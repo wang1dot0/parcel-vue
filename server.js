f6dd957bd140e8083e9d384705d1ff0e2d593346
wangguansheng <wang.guansheng@navercorp.com>
2019-02-02 14:15:46 +0800

    Add middleware morgan

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const Bundler = require('parcel-bundler');
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
 const file = path.resolve(__dirname, './public/index.html');
-
+const isProduction = process.env.NODE_ENV === 'production' ? true : false;
 // Copied from https://zh.parceljs.org/api.html
 const options = {};
 
 const bundler = new Bundler(file, options);
 
-app.use(bundler.middleware());
+if (!isProduction) {
+  app.use(morgan('dev'));
+}
 
+app.use(bundler.middleware());
 app.listen(8081);
 
 bundler.on('buildStart', () => {
